a5281440adb7b765db908bb75e6b4f5cb12cbf3a
JackDoobies <p467103@tafe.wa.edu.au>
2024-10-03 21:35:57 +0800

    These are the updated php files. Significant changes were made from the previous iteration.
    Changes made based upon the assignment specifications, upon closer inspection it was revealed to me that the DB had to be 1nf.
    Curious...

---
+++ at3/questions/question7.php
@@ -0,0 +1,118 @@
+<!-- 
+P467103
+JACK DU BOULAY
+07/09/2024
+-->
+
+<!-- PHP CODE -->
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Question 7</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="../mystyle.css">
+    </head>
+
+    <body class="text-center">
+        <header class="sticky-top">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-3">
+                <div class="container-fluid">
+                    <button class="navbar-toggler active" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation"> 
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="../index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="../contacts.html">Contacts</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownMenu" role="button" data-bs-toggle="dropdown" aria-expanded="false">Content</a>
+                                    <ul class="dropdown-menu active" aria-labelledby="navbarDropdownMenu">
+                                    <li><a class="dropdown-item" href="question1.php">Question 1</a></li>
+                                    <li><a class="dropdown-item" href="question2.php">Question 2</a></li>
+                                    <li><a class="dropdown-item" href="question3.php">Question 3</a></li>
+                                    <li><a class="dropdown-item" href="question4.php">Question 4</a></li>
+                                    <li><a class="dropdown-item" href="question5.php">Question 5</a></li>
+                                    <li><a class="dropdown-item" href="question6.php">Question 6</a></li>
+                                    <li><a class="dropdown-item" href="question7.php">Question 7</a></li>
+                                    <li><a class="dropdown-item" href="question8.php">Question 8</a></li>
+                                    <li><a class="dropdown-item" href="question9.php">Question 9</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+        <main role="main" class="mainClass">
+            <div class="container">
+                <h1 class="mt-5">Questions and Answers</h1>
+                <table class="table table-bordered mt-3">
+                    <?php
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "";
+                        $dbname = "AT3_QUESTIONS_DB";
+                        
+                        try {
+                            // Create connection using PDO
+                            $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
+                            // Set the PDO error mode to exception
+                            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                            
+                            // SELECT RESULT WITH THIS VAR
+                            $question_id = 7;
+                            
+                            // SQL query to fetch the specific row - these are the columns to collect from and where Question_ID = :question_id 
+                            $sql = "SELECT Question, Description, Answer FROM QUESTION WHERE Question_ID = :question_id";
+                            $stmt = $conn->prepare($sql);
+                            $stmt->bindParam(':question_id', $question_id, PDO::PARAM_INT);
+                            $stmt->execute();
+                            
+                            // Fetch the specific row
+                            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                            
+                            if ($row) {
+                                echo '<div class="accordion-item">';
+                                echo '<h2 class="accordion-header" id="heading1">';
+                                echo '<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">';
+                                echo nl2br(htmlspecialchars_decode($row["Question"]));
+                                echo '</button>';
+                                echo '</h2>';
+                                echo '<div id="collapse1" class="accordion-collapse collapse" aria-labelledby="heading1" data-bs-parent="#accordionExample">';
+                                echo '<div class="accordion-body">'; 
+                                echo '<strong>Description:</strong> ' . '<br>'. nl2br(htmlspecialchars_decode($row["Description"])) . '<br><br>';
+                                echo '<strong>Answer:</strong> '. '<br>' . nl2br(htmlspecialchars_decode($row["Answer"]));
+                                echo '</div>';
+                                echo '</div>';
+                                echo '</div>';
+                            } else {
+                                echo "<div class='alert alert-warning' role='alert'>Could not find result for question {$question_id}</div>"; // Could not find row at question_id
+                            }
+                        } catch(PDOException $e) { 
+                            echo "Connection failed: " . $e->getMessage();
+                        }
+                        $conn = null;
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </main>
+
+        <footer>
+            <!-- place footer here -->
+        </footer>
+
+        <!-- Bootstrap JavaScript Libraries -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </body>
+</html>
--- at3_questions_db.sql
+++ at3_questions_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 01, 2024 at 04:45 PM
+-- Generation Time: Oct 03, 2024 at 03:33 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,53 +29,25 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `question` (
   `Question_ID` int(11) NOT NULL,
-  `Question` varchar(255) NOT NULL,
-  `Description_ID` int(11) NOT NULL,
-  `Answer_ID` int(11) NOT NULL
+  `Question` varchar(256) NOT NULL,
+  `Description` varchar(1024) NOT NULL,
+  `Answer` varchar(4096) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `question`
 --
 
-INSERT INTO `question` (`Question_ID`, `Question`, `Description_ID`, `Answer_ID`) VALUES
-(1, 'Question 1.\r\nOrganisational Requirements <br>\r\n', 1, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `q_answer`
---
-
-CREATE TABLE `q_answer` (
-  `Answer_ID` int(11) NOT NULL,
-  `Answer` varchar(2047) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `q_answer`
---
-
-INSERT INTO `q_answer` (`Answer_ID`, `Answer`) VALUES
-(1, 'Code of Conduct: details acceptable organisational and ethical and professional behaviours that employees must follow.  This is important to ensure that team task \r\n\r\nCITE Corporate Social Responsibility:  details the organisations social responsibilities and what they consider is important for the community, the people within that community, sustainability and their accountability.  This determines the culture of the workplace environment \r\n\r\nThe Equity and Diversity Policy:  ensures that the employees of the organization are aware of their rights and responsibilities in creating and maintaining a work environment which encourages and respects equal employment opportunity and diversity and is free from discrimination, harassment, victimization and bullying.  This is fulfils the EEO Act 2000, Disability Opportunity Act 1984 and Age Discrimination Act 2004. \r\n\r\nThe OSH Policy – addresses the OSH of the employees by assisting them to understand their responsibilities and duties with regard to safety and health as well as identifying the responsibilities and duties of the organization as an employer. ');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `q_description`
---
-
-CREATE TABLE `q_description` (
-  `Description_ID` int(11) NOT NULL,
-  `Description` varchar(1023) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `q_description`
---
-
-INSERT INTO `q_description` (`Description_ID`, `Description`) VALUES
-(1, 'When working with teams from other Divisions, describe the organizational policies that you will refer to, to ensure that your project aligns with the organization’s requirements and the importance of each:\r\n\r\n    1. Workplace policies\r\n    2. Code of conduct\r\n    3. Reputation and culture\r\n    4. Legislative requirements (EEO, WHS).\r\n');
+INSERT INTO `question` (`Question_ID`, `Question`, `Description`, `Answer`) VALUES
+(1, 'Question 1\r\nOrganisational Requirements ', 'When working with teams from other Divisions, describe the organizational policies that you will refer to, to ensure that your project aligns with the organization’s requirements and the importance of each: \r\n\r\n1. Workplace policies \r\n2. Code of conduct \r\n3. Reputation and culture \r\n4. Legislative requirements (EEO, WHS) ', 'Code of Conduct: details acceptable organisational and ethical and professional behaviours that employees must follow.  This is important to ensure that team task \r\n\r\nCITE Corporate Social Responsibility:  details the organisations social responsibilities and what they consider is important for the community, the people within that community, sustainability and their accountability.  This determines the culture of the workplace environment \r\n\r\nThe Equity and Diversity Policy:  ensures that the employees of the organization are aware of their rights and responsibilities in creating and maintaining a work environment which encourages and respects equal employment opportunity and diversity and is free from discrimination, harassment, victimization and bullying.  This is fulfills the EEO Act 2000, Disability Opportunity Act 1984 and Age Discrimination Act 2004. \r\n\r\nThe OSH Policy – addresses the OSH of the employees by assisting them to understand their responsibilities and duties with regard to safety and health as well as identifying the responsibilities and duties of the organization as an employer. '),
+(2, 'Question 2  \r\nTeam facilitation techniques ', 'Working with teams can be challenging. What does establish a team performance plan mean and what are the benefits? \r\n\r\nHow would you handle a potential risk or safety hazard to ensure that it did not effect the teams performance?  Provide an example of the risk (eg, second wave of COVID) and the strategy that you would implement. \r\n\r\nThe project’s final deadline is nearing, and it is possible that the project will not be complete unless an unethical alternative suggested by one of the team members is implemented.  What is your approach to tackle this situation? ', 'Prioritise the tasks.   Needs to drive everything. Start at the top of the list and begin allocating work from there. That list should be based on the team’s and the organization’s goals. This has to be the first consideration in terms of how you distribute work. If a project is a top priority and somebody is available to do that work, they should be tasked with that work. \r\n\r\nSkill Sets - Evaluate the skill set of the people who you’re thinking about distributing the work to. If they have the right skill set, you’re going to get a high quality result. The end product will be something that meets your customer’s needs. This also reduces the likelihood of people failing because you’re not giving them work that they don’t have the skill set to perform. You’re giving them something they can be successful with. \r\n\r\nUndertaking Performance Management with individuals will provide insight to their goals.  Be transparent and communicate that they have been singled out as a protégé, but don’t make any guarantees as the situation can change due to circumstances.  Invest in the professional development of those that you select as a successor. '),
+(3, 'Question 3 \r\nMentoring and coaching techniques ', 'Coaching and mentoring are development approaches based on the use of one to one conversation to enhance and individuals’ skills, knowledge or work performance.  Describe 2 techniques in coaching and mentoring that you will use with your team to support the members. ', 'Listening \r\n\r\nCoaching is not about the coach, but rather about those that they are coaching.  Listen to their thoughts, needs and concerns.  Listen in order to: \r\n\r\nnote key points which you can summarise for your coachee to help maintain clarity \r\n\r\npick up on any unfocused statements and then encourage more clarity \r\n\r\nnote the way your coachee responds to determine any underlying issues through further questioning. \r\n\r\nAsking open-ended question that focus on three levels \r\n\r\nFirst level:  describing the issue \r\n\r\nSecond level:  strategic questions – digging deeper \r\n\r\nThird level: strategic questions – helping it change \r\n\r\nMaking observations \r\n\r\nReflect things you observe about a person back to them in a neutral, non-judgement way.  These are only useful when validated by expressing them to the person involved and getter their agreement that the observation is correct and valid.  A well-stated observation may then lead into a coaching or performance conversation. '),
+(4, 'Question 4 \r\nConflict resolution ', 'When conflict is resolved effectively, it leads to many benefits, such as accomplishing goals and strengthening relationships. Broadly outline a strategy on how you would resolve a conflict within your team. ', 'Identify common goals \r\n\r\nSearch for agreement \r\n\r\nConsider the range of solutions and possibilities \r\n\r\nQuestion our beliefs \r\n\r\nCommit to resolving issues and problems as a group '),
+(5, 'Question 5 \r\nCommunication methods and styles ', 'Project success depends on effective communication.  Steady communication from leadership can improve morale and engagement. Describe the different methods and styles of communication that you will use with your team. ', 'Methods: \r\n\r\n- Verbal communication \r\n- Non-verbal communication (body language) \r\n- Written communication \r\n- Listening \r\n- Visual communication \r\n\r\nStyle used with the team:  \r\n\r\n- Assertive (achieves goals without hurting others, emotionally expressive, protects own rights and rights of others, speaks with a balanced tone and volume) '),
+(6, 'Question 6  \r\nCross cultural communication ', 'We work and live in a diverse community that has different cultures and individuals with special needs or disabilities. Describe the principles of communication for these groups and how you would apply these principles with your team. ', 'Maintain etiquette – many cultures have specific etiquette around the way they communicate.  Research the target culture. \r\n\r\nAvoid slang – Nonactive English speakers have a comprehensive understanding of English but not slang idiom and sayings.  They may understand the individual words you have said but not the context or meaning. \r\n\r\nSpeak slowly – Modulating your pace will hep as will speaking clearly and pronouncing your words properly. \r\n\r\nKeep it simple – avoid using unnecessary large words. \r\n\r\nThe same techniques would be used when communicating with individuals that have special needs or disabilities but would ask the person what will help with communication as there are different ways to communicate and be polite and patient. '),
+(7, 'Question 7  \r\nProfessionalism ', 'A good team leader leads by example.  Describe the professional behaviours that you would role model as a leader for your team ', 'Being grounded in ethics and integrity – doing the right things for the right reasons \r\n\r\nBuilding trust – doesn’t come automatically but is earned.  Take actions that gain respect. \r\n\r\nBringing others along – help others grow and achieve.  Work with people to help them uncover what they do best and ways to strengthen their assets \r\n\r\nInspiring those around you – have a vision that motivates people to follow and expressing it with passion and energy, backing it was strong beliefs and values that count \r\n\r\nMaking decisions – especially the hard decisions with authority and confidence. \r\n\r\nRewarding achievement – recognize hard work and deliver what you promise when that happens. '),
+(8, 'Question 8  \r\nWorkplace Contingencies ', 'Personal events can often place strain on teams and contingencies need to be put in place for: \r\n\r\nUnplanned leave or absence of workers \r\n\r\nRe-allocation of work tasks \r\n\r\nSuccession planning for important team roles \r\n\r\nDescribe the contingencies that you have planned for each of the above to ensure that the project objectives can be met. ', 'Establish team performance \r\n\r\nIdentify the desired performance levels of the team \r\n\r\nIdentify how these performance levels will be achieved \r\n\r\nProvide guidance and direction to the team \r\n\r\nMeasure progress towards the desired performance levels \r\n\r\nAs an example of COVID where face to face meetings where no longer possible and group work still had to continue.  I would change the meetings to a suitable medium (continue with virtual meetings) but have more check-ins with individual team members to ensure that they don’t feel isolated. \r\n\r\nReiterate the code of conduct and organization policy to the entire team. Be firm about not following that path and counsel the individual member. '),
+(9, 'Question 9  \r\nTeamwork Challenges ', 'Cohesive teams are better able to accomplish tasks, solve problems and create innovation.  As a team leader describe several strategies that you will use to develop team cohesion and effectiveness in your project team. ', 'Communicate with your staff \r\n\r\nBe clear with your staff about the purpose of their work, expectations and common goals. You can easily achieve this by having regular office meetings and coming up with a mission statement for your office that encourages a team mentality. By not communicating with employees, you increase the risk of misunderstanding, assumptions, apathy and lack of loyalty. \r\n\r\nCommit to staff development \r\n\r\nSavvy business leaders are always putting resources towards the continued education of their staff members, and helping to develop their skills that will enhance their value to the team. Sending your staff (and yourself) to seminars on communication, emotional intelligence, team building and leadership training are worthwhile as well. Remember to follow up on these courses by giving staff your ongoing support by ensuring employees use their new skills, encouraging them to be proactive, and implementing effective new policies. \r\n\r\nOffer feedback \r\n\r\nYour staff members should always know where they stand with respect to your company, and by providing them with both casual and formal evaluation you will effectively be showing them that they’re valued, respected and that their work is acknowledged. Many business owners make the mistake of only providing feedback when it’s negative, but providing plenty of positive feedback will build the confidence of your staff members and will make it more effective when you do need to criticise. \r\n\r\nEncourage collaboration \r\n\r\nEvery office needs some collaboration among staff members in order to make the work happen, but there are plenty of benefits to making that collaboration more effective. By creating a space where employees can get to know each other — even if they don’t work closely that often — then you will create a space that is more comfortable and that encourages more creative thinking and brainstorming. Having staff members that know each other and that work well together means that better work will get done more quickly, and more innovation will come of it. \r\n\r\nBe consistent \r\n\r\nNo matter how many staff members you have, it’s important that you try to be consistent in your decision-making. Having strong office policies means you don’t “play favourites” and you treat each employee the same, which will make them all feel more valued. Communicate decisions clearly and openly so that everyone knows them at the same time, and handle similar situations involving staff members the same way every time so that each employee knows they are as important. \r\n\r\nStrong teams don’t just appear magically, as you have to work hard to improve communication and respect amongst your staff members. If you really work at it, don’t be surprised if you start noticing positive changes quickly. ');
 
 --
 -- Indexes for dumped tables
@@ -85,21 +57,7 @@ INSERT INTO `q_description` (`Description_ID`, `Description`) VALUES
 -- Indexes for table `question`
 --
 ALTER TABLE `question`
-  ADD PRIMARY KEY (`Question_ID`),
-  ADD KEY `FK_DESC` (`Description_ID`),
-  ADD KEY `FK_ANSW` (`Answer_ID`);
-
---
--- Indexes for table `q_answer`
---
-ALTER TABLE `q_answer`
-  ADD PRIMARY KEY (`Answer_ID`);
-
---
--- Indexes for table `q_description`
---
-ALTER TABLE `q_description`
-  ADD PRIMARY KEY (`Description_ID`);
+  ADD PRIMARY KEY (`Question_ID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -109,30 +67,7 @@ ALTER TABLE `q_description`
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `Question_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `q_answer`
---
-ALTER TABLE `q_answer`
-  MODIFY `Answer_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `q_description`
---
-ALTER TABLE `q_description`
-  MODIFY `Description_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `question`
---
-ALTER TABLE `question`
-  ADD CONSTRAINT `FK_ANSW` FOREIGN KEY (`Answer_ID`) REFERENCES `q_answer` (`Answer_ID`),
-  ADD CONSTRAINT `FK_DESC` FOREIGN KEY (`Description_ID`) REFERENCES `q_description` (`Description_ID`);
+  MODIFY `Question_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
